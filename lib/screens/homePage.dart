13d312c1cf2be305e5511eaceffe4013973ec47d
Dadahon0019 <95474203+dadaxon0019@users.noreply.github.com>
2022-06-29 16:04:06 +0300

    create Traveling App

---
+++ lib/screens/homePage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff031F2B),
+      body: Center(
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 34,horizontal: 32),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Hello. Hazar ',style: TextStyle(
+                              color: Color(0xffD6D2D2),
+                            fontSize: 12,
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Text('Let’s Travel',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                letterSpacing: 1,
+                                fontWeight: FontWeight.w600
+                              ),
+                          )
+                        ],
+                      ),
+                      Expanded(child: Container()),
+                      Image(image: AssetImage('assets/account_img.png'))
+                    ],
+                  )
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
