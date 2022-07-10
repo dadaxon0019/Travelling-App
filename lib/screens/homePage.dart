248403ff2c3185e744cf25be5758d1e152f6483a
Dadahon0019 <95474203+dadaxon0019@users.noreply.github.com>
2022-07-10 08:19:53 +0300

    create welcome page

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -7,7 +7,6 @@ import 'package:travelling_app/widgets/inputWidget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:travelling_app/widgets/slider.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -16,15 +15,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late bool clickSlider = clickSlider;
 
-    late bool clickSlider = clickSlider;
+  final jsonString = {"name": "John Smith", "email": "john@example.com"};
 
-  final jsonString = {
-    "name": "John Smith",
-    "email": "john@example.com"
-  };
-
- bool starIsDone2 = true;
+  bool starIsDone2 = true;
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +28,7 @@ class _HomePageState extends State<HomePage> {
       body: ListView(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             child: Column(
               children: [
                 Container(
@@ -45,22 +40,24 @@ class _HomePageState extends State<HomePage> {
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text('Hello. Dadaxon',style: TextStyle(
+                              Text(
+                                'Hello. Dadaxon',
+                                style: TextStyle(
                                   color: Color(0xffD6D2D2),
-                                fontSize: 12,
+                                  fontSize: 12,
                                 ),
                               ),
-                              SizedBox(height: 10,),
+                              SizedBox(
+                                height: 10,
+                              ),
                               Text('Let’s Travel',
                                   style: GoogleFonts.poppins(
                                     textStyle: TextStyle(
                                         color: Colors.white,
                                         fontSize: 24,
                                         letterSpacing: 1,
-                                        fontWeight: FontWeight.w600
-                                    ),
-                                  )
-                              )
+                                        fontWeight: FontWeight.w600),
+                                  ))
                             ],
                           ),
                           Expanded(child: Container()),
@@ -71,20 +68,20 @@ class _HomePageState extends State<HomePage> {
                     ],
                   ),
                 ),
-               Container(
-                 height: 35,
-                 child: ListView.builder(
-                     itemBuilder: (BuildContext context,int index){
-                   return ListItem(
-                   );
-                  },
-                   itemCount: sliderTitles.length,
-                   scrollDirection: Axis.horizontal,
-
-                 ),
-               ),
-                SizedBox(height: 32,),
-              Container(
+                Container(
+                  height: 35,
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListItem();
+                    },
+                    itemCount: sliderTitles.length,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Container(
                   child: Column(
                     children: [
                       Row(
@@ -95,27 +92,30 @@ class _HomePageState extends State<HomePage> {
                                 color: Colors.white,
                                 fontSize: 16,
                                 fontWeight: FontWeight.w500,
-                                letterSpacing: 0.6
-                            ),
+                                letterSpacing: 0.6),
                           ),
                         ],
                       ),
-                      SizedBox(height: 16,),
+                      SizedBox(
+                        height: 16,
+                      ),
                       Container(
                         height: 178,
                         child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount: namesTrips.length,
-                          itemBuilder: (BuildContext context,int index){
+                          itemBuilder: (BuildContext context, int index) {
                             return ImageSLider(
                               nameTrip: namesTrips[index],
                               description: descriptionTrips[index],
-                              imgUrl:  imgUrl[index],
+                              imgUrl: imgUrl[index],
                             );
                           },
                         ),
                       ),
-                      SizedBox(height: 16,),
+                      SizedBox(
+                        height: 16,
+                      ),
                       Row(
                         children: [
                           Text(
@@ -123,24 +123,25 @@ class _HomePageState extends State<HomePage> {
                             style: TextStyle(
                                 fontSize: 17,
                                 color: Colors.white,
-                                fontWeight: FontWeight.w500
-                            ),
+                                fontWeight: FontWeight.w500),
                           ),
                         ],
                       ),
-                      SizedBox(height: 16,),
+                      SizedBox(
+                        height: 16,
+                      ),
                       Container(
                         height: nameCard.length * 156,
                         child: ListView.builder(
                           physics: const NeverScrollableScrollPhysics(),
                           itemCount: nameCard.length,
-                          itemBuilder: (BuildContext context,int index){
+                          itemBuilder: (BuildContext context, int index) {
                             return Padding(
                               padding: const EdgeInsets.only(bottom: 15),
                               child: FeaturedCard(
                                 nameCard: nameCard[index],
                                 aboutCard: aboutCard[index],
-                                imgCard:  imgCard[index],
+                                imgCard: imgCard[index],
                                 raitingCard: raitingCard[index],
                               ),
                             );
--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travelling_app/screens/homePage.dart';
 
-
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -11,95 +10,88 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Color(0xff031F2B),
       body: Container(
         width: double.infinity,
-        padding: EdgeInsets.only(top: 164,bottom: 24,right: 24,left: 24),
+        padding: EdgeInsets.only(top: 164, bottom: 24, right: 24, left: 24),
         color: Color(0xff031F2B),
         child: Column(
           children: [
-            Image(
-                image: AssetImage('assets/LoginImg.png')
+            Image(image: AssetImage('assets/LoginImg.png')),
+            SizedBox(
+              height: 28,
             ),
-            SizedBox(height: 28,),
             Text(
               'Escape the ordinary life',
               style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0.8, color: Colors.white
-              ),
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.8,
+                  color: Colors.white),
             ),
-            SizedBox(height: 7,),
+            SizedBox(
+              height: 7,
+            ),
             Text(
               'Discover great experiences around you \nand make you live interesting!',
               style: TextStyle(
                   fontSize: 14,
                   height: 1.4,
                   fontWeight: FontWeight.w400,
-                  letterSpacing: 0.8, color: Color(0xffD6D2D2)
-              ),
+                  letterSpacing: 0.8,
+                  color: Color(0xffD6D2D2)),
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 157,),
+            SizedBox(
+              height: 157,
+            ),
             Column(
               children: [
                 Container(
                   clipBehavior: Clip.hardEdge,
                   height: 52,
                   width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10)
-                  ),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
                   child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                     primary: Color(0xff5EDFFF),
-
-                    ),
-                      onPressed: (){},
-                      child: Text(
-                          'Get Started',
-                          style:TextStyle(
-                              fontSize: 14,
-                              color: Color(0xff031F2B)
-                          )
-                      )
-                  ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xff5EDFFF),
+                      ),
+                      onPressed: () {},
+                      child: Text('Get Started',
+                          style: TextStyle(
+                              fontSize: 14, color: Color(0xff031F2B)))),
                 ),
-                SizedBox(height: 16,),
+                SizedBox(
+                  height: 16,
+                ),
                 Container(
                   clipBehavior: Clip.hardEdge,
                   height: 52,
                   width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10)
-                  ),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
                   child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)
-                        ),
+                            borderRadius: BorderRadius.circular(10)),
                         side: BorderSide(
                           color: Color(0xff5EDFFF).withOpacity(0.6),
                           width: 2,
                         ),
-                        primary:Color(0xff031F2B),
+                        primary: Color(0xff031F2B),
                       ),
-                      onPressed: (){
+                      onPressed: () {
                         Navigator.pushReplacement(
                           context,
                           PageRouteBuilder(
                             pageBuilder: (c, a1, a2) => HomePage(),
-                            transitionsBuilder: (c, anim, a2, child) => FadeTransition(opacity: anim, child: child),
+                            transitionsBuilder: (c, anim, a2, child) =>
+                                FadeTransition(opacity: anim, child: child),
                             transitionDuration: Duration(milliseconds: 1500),
                           ),
                         );
                       },
-                      child: Text(
-                          'Log In',
-                          style:TextStyle(
-                              fontSize: 14,
-                              color: Color(0xff5EDFFF)
-                          )
-                      )
-                  ),
+                      child: Text('Log In',
+                          style: TextStyle(
+                              fontSize: 14, color: Color(0xff5EDFFF)))),
                 ),
               ],
             )
