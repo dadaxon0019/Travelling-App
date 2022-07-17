e460971e8c02acddd028d10c4ddd1d5dc1d3681f
Dadahon0019 <95474203+dadaxon0019@users.noreply.github.com>
2022-07-17 13:50:04 +0300

    finishing tabBarIndicator

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travelling_app/json/FeaturedCardInfo.dart';
 import 'package:travelling_app/json/Trips.dart';
+import 'package:travelling_app/misc/colors.dart';
 
 import 'package:travelling_app/widgets/FeaturedCard.dart';
 import 'package:travelling_app/widgets/imagesSlider.dart';
@@ -83,18 +84,47 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
                   ),
                 ),*/
                 Container(
-                  child: TabBar(
-                    tabs: [
-                      Tab(text: 'Places',),
-                      Tab(text: 'Inspiration',),
-                      Tab(text: 'Emotions',)
-                    ],
+
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: TabBar(
+                      labelPadding: const EdgeInsets.only(left: 20,right: 20),
+                      controller: _tabController,
+                      isScrollable: true,
+                      indicatorSize: TabBarIndicatorSize.label  ,
+                      indicator: CircleTabIndicator(color: AppColors.mainColor,radious: 4),
+                      tabs: [
+                        Tab(text: 'Places',),
+                        Tab(text: 'Inspiration',),
+                        Tab(text: 'Emotions',)
+                      ],
+                    ),
                   ),
                 ),
                 Container(
+                  height: 300,
+                  width: double.maxFinite,
                   child: TabBarView(
+                    controller: _tabController,
                     children: [
-                      Text('hi'),
+                      ListView.builder(
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            width: 200,
+                            height: 300,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white,
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/mountain.jpeg'
+                                    ),
+                                    fit: BoxFit.cover
+                                )
+                            ),
+                          ),
+                        },
+                      ),
                       Text('There'),
                       Text('Bye')
                     ],
@@ -180,4 +210,34 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
       ),
     );
   }
+}
+
+
+class CircleTabIndicator extends Decoration{
+  final Color color;
+  double radious;
+  CircleTabIndicator({required this.color,required this.radious});
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePainter(color:color,radious:radious);
+  }
+
+}
+
+class _CirclePainter extends BoxPainter{
+  final Color color;
+  double radious;
+  _CirclePainter({required this.color,required this.radious});
+  @override
+  void paint(Canvas canvas, Offset offset,
+      ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color=color;
+    _paint.isAntiAlias=true;
+    final Offset circleOffset = Offset(configuration.size!.width/2 - radious/2, configuration.size!.height-radious);
+
+    canvas.drawCircle(offset + circleOffset, radious, _paint);
+    
+  }
+
 }
